3f3548c35755574cd34a9ae5f6d77d9fe0bb3d52
Amandeep Bassi <amandeep.s.bassi@gmail.com>
2022-07-18 21:01:44 -0400

    added db connection

--- app/src/main/java/com/attendance/contactless/UsageActivity.java
+++ app/src/main/java/com/attendance/contactless/UsageActivity.java
@@ -4,8 +4,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class UsageActivity extends AppCompatActivity {
     private Button isStudentButtonBtn;
@@ -20,6 +28,23 @@ public class UsageActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(UsageActivity.this, MainActivity.class));
+                String URL = "http://192.168.4.2/myproject/checkconnection.php";
+                RequestQueue queue;
+
+                queue = Volley.newRequestQueue(UsageActivity.this);
+
+                StringRequest stringrequest = new StringRequest(Request.Method.GET, URL, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Toast.makeText(getApplicationContext(), "Response from server"+response,Toast.LENGTH_LONG).show();
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(), "Error:"+error.getMessage(),Toast.LENGTH_LONG).show();
+                    }
+                });
+                queue.add(stringrequest);
             }
 
         });
@@ -29,6 +54,23 @@ public class UsageActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(UsageActivity.this, TeacherActivity.class));
+                String URL = "http://192.168.4.2/myproject/checkconnection.php";
+                RequestQueue queue;
+
+                queue = Volley.newRequestQueue(UsageActivity.this);
+
+                StringRequest stringrequest = new StringRequest(Request.Method.GET, URL, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Toast.makeText(getApplicationContext(), "Response from server"+response,Toast.LENGTH_LONG).show();
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(), "Error:"+error.getMessage(),Toast.LENGTH_LONG).show();
+                    }
+                });
+                queue.add(stringrequest);
             }
         });
     }
