0f97f1bfc36ec91376f5d3d58bbfe0b56a6d050d
Siyayi <sndemera1@student.gsu.edu>
2022-07-23 17:38:19 -0400

    connecting studentId to DB (not finished-need work)

--- app/src/main/java/com/attendance/contactless/MainActivity.java
+++ app/src/main/java/com/attendance/contactless/MainActivity.java
@@ -13,6 +13,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //mydb = new DBHelper(this);
+        //ArrayList array_list = mydb.getAllContacts();
+        //ArrayAdapter arrayAdapter=new ArrayAdapter(this,android.R.layout.simple_list_item_1, array_list);
+
         Spinner mySpinner = (Spinner) findViewById(R.id.TeacherList);
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
@@ -20,4 +24,20 @@ public class MainActivity extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         mySpinner.setAdapter(adapter);
     }
+    /* Connecting the UI to Database
+
+    obj = (ListView)findViewById(R.id.TeacherList);
+    obj.setAdapter(arrayAdapter);
+    obj.setOnItemClickListener(new OnItemClickListener(){
+        public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,long arg3) {
+                // TODO Auto-generated method stub
+                int id_To_Search = arg2 + 1;
+                Bundle Spinner = new Spinner();
+                Spinner.putInt("id", id_To_Search);
+                Intent intent = new Intent(getApplicationContext(),DisplayContact.class);
+                intent.putExtras(Spinner);
+                startActivity(intent);
+             }
+       }
+     */
 }
